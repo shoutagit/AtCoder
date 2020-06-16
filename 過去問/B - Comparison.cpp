ecf3ca57d21060632226eb87f9ada2568166be74
shoutagit <shoutaxxx1998@yahoo.co.jp>
2020-06-16 14:45:14 +0900

    atcoder

--- 過去問/B - Comparison.cpp
+++ 過去問/B - Comparison.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+// a,bは最大10^100
 int main(){
+    //intではなくlong doubleを使えばいい
+    long double a, b;
+    cin >> a >> b;
+    if(a>b){
+        cout << "GREATER" << endl;
+    }
+    else if (a<b){
+        cout << "LESS" << endl;
+    }
+    else{
+        cout << "EQUAL" << endl;
+    }
     
     
 }
