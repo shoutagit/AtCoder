8615eb47fbbd8525fc7af062675301d7e206204a
shoutagit <shoutaxxx1998@yahoo.co.jp>
2020-06-19 15:33:17 +0900

    atcoder

---
+++ 過去問/B - Cut and Count.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int ans=0;
+
+}
