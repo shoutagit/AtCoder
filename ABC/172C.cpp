57c4ad55864b2603cb75d2094efcd739ba023382
shoutagit <shoutaxxx1998@yahoo.co.jp>
2020-07-02 22:28:59 +0900

    atcoder

--- ABC/172C.cpp
+++ ABC/172C.cpp
@@ -6,34 +6,39 @@ using ll = long long;
 #define all(x) (x).begin(),(x).end()
 const ll INF = 1LL << 60;
 
+//単調増加なので二分探索でできる
+//以下は尺取法を行っている
 int main(){
     ll n, m, k;
     cin >> n >> m >> k;
-    vector<ll> a(n);
-    vector<ll> b(m);
+    vector<int> a(n);
+    vector<int> b(m);
     rep(i,n) cin >> a[i];
     rep(i,m) cin >> b[i];
 
+    ll t=0;
+    rep(i,m) t+= b[i];
+
+    int j=m;
     int ans=0;
-    ll acount=0;
-    ll bcount=0;
-    while(true){
-        if(acount==n && bcount ==m){
-            cout << n+m << endl;
-            return 0;
+    //i(A側)とj(B側)の2つのカーソルを追う
+    rep(i,n+1){
+        //合計時間tがkを超えていたら１つずつ諦める
+        while(j>0 && t>k){
+            --j;
+            t -= b[j];
         }
-        else if(a[acount]<=b[acount] || bcount==m){
-            k -= a[acount];
-            if(k<0) break;
-            acount ++;
-        }
-        else if(a[acount]>b[bcount] || acount==n){
-            k-= b[bcount];
-            if(k<0) break;   
-            bcount ++;
-        }
+        //もう諦められる本がない(j=0)かつt>kならbreak
+        if(t>k) break;
+        ans = max(ans,i+j);
+        //配列外参照を防ぐためにbreakで誤魔化している
+        if(i==n) break;
+        t+= a[i];
     }
-    cout << acount+bcount << endl;
+
+    cout << ans << endl;
+    return 0;
+   
 
 
 }
