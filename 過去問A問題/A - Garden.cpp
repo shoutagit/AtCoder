0253cfb94ef2dfc83449585bfaebd2c356e95422
shoutagit <shoutaxxx1998@yahoo.co.jp>
2020-07-18 00:27:15 +0900

    atcoder

---
+++ 過去問A問題/A - Garden.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+const ll INF = 1LL << 60;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << (a-1)*(b-1) << endl;
+    return 0;
+}
